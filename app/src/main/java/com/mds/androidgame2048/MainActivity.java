a5591dbe7b64510e4508efdcdcdad7ac86d02bab
shirozuo <shiroyonago@gmail.com>
2021-07-21 06:21:17 +0300

    Add OneSignal

--- app/src/main/java/com/mds/androidgame2048/MainActivity.java
+++ app/src/main/java/com/mds/androidgame2048/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.onesignal.OneSignal;
+
+
 public class MainActivity extends AppCompatActivity {
+    private static final String ONESIGNAL_APP_ID = "05401944-d660-4d54-b1d5-336f7df97b13";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
     }
 }
